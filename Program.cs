26f6fea2cb322b52e6429c894e6b4669e619bd42
Miranda00 <diegomiranda0799@gmail.com>
2022-03-12 14:46:34 -0600

    Agregando codigo

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace ComandosSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //SALIDA DE TEXTO SiN ESPACIO
+            Console.Write("Esta es una salida de texto sin enter");
+            //SALIDA DE TEXTON CON ENTER
+            Console.WriteLine ("Esta es una salida de texto con enter");
+
+            //Declarando una variable; tipos: int, double, char, bool, string, etc
+            int age,carnet;
+            var name = "";
+            var lastName = "";
+            var carrer = "";
+
+            //Recivir datos de parte del usuario. C# recibe entradas como texto, hay que convertit.
+            Console.Write ("\nIngrese su nombre: ");
+            name = Console.ReadLine();
+            Console.Write ("Ingrese su apellido: ");
+            lastName = Console.ReadLine();
+            Console.Write ("Ingresela  carrera que esta cursando: ");
+            carrer = Console.ReadLine();
+            Console.Write ("Ingrese su edad: ");
+            age = Convert.ToInt32(Console.ReadLine());            
+            Console.Write ("Ingrese su carnet: ");
+            carnet = Convert.ToInt32(Console.ReadLine());
+
+            //Concatenar y salida de txto usando datos otorgados por el usuario 
+            System.Console.WriteLine("\nBienvenido");
+            System.Console.WriteLine($"Nombre: {name} \nApellido: {lastName} \nCarnet: {carnet} \nCarrera: {carrer} \nEdad: {age}");
+
+            //FLUJOS (?)
+
+            
         }
     }
 }
